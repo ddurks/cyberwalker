2a6a20283debb6c830ddd7306fa331f5170d4340
David Durkin <onlinedavid@Davids-MacBook-Air.local>
2023-01-17 21:07:08 -0600

    more assets + infinite terrain

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ if (
   /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
     navigator.userAgent
   ) ||
-  /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+  /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(14|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|14|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|84|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
     navigator.userAgent.substr(0, 4)
   )
 ) {
@@ -32,6 +32,16 @@ export const back = "back";
 export const lft = "left";
 export const rt = "right";
 export const JOY_DIRS = [fwd, back, lft, rt];
+export const BUILDING_TYPES = ['square', 'cylinder', 'sphere', 'pyramid'];
+export const FOLIAGE_TYPES = ['bush1', 'bush2', 'tree1', 'tree2'];
+export var WORLD_BOUNDARY_X = [-42, 42];
+export var WORLD_BOUNDARY_Z = [-42, 42];
+var current_region = {
+  x: 0,
+  z: 0
+}
+var regionsList = [];
+const REGION_SIZE = 84;
 
 export class CharacterControls {
 
@@ -100,7 +110,6 @@ export class CharacterControls {
 
       // move model & camera
       body.velocity.set(this.walkDirection.x * this.walkVelocity, 0, this.walkDirection.z * this.walkVelocity);
-      body.linearDamping = 0.999;
     } else {
       this.walkStart = null;
       this.walkVelocity = this.defaultWalkVelocity;
@@ -135,19 +144,10 @@ export class CharacterControls {
     this.mixer.update(delta * speedMultiplier);
   }
 
-  updateCameraTarget(moveX, moveZ) {
+  updateCameraTarget() {
     // calculate camera movement
-    var moveX, moveZ;
-    if (body.velocity.x > 0) {
-      moveX = Math.abs(body.position.x - body.lastPosition.x);
-    } else {
-      moveX = - Math.abs(body.position.x - body.lastPosition.x);
-    }
-    if (body.velocity.z > 0) {
-      moveZ = Math.abs(body.position.z - body.lastPosition.z);
-    } else {
-      moveZ = - Math.abs(body.position.z - body.lastPosition.z);
-    }
+    let moveX = (body.velocity.x > 0 ? 1 : -1) * Math.abs(body.position.x - body.lastPosition.x);
+    let moveZ = (body.velocity.z > 0 ? 1 : -1) * Math.abs(body.position.z - body.lastPosition.z);
     body.lastPosition = { x: body.position.x, y: body.position.y, z: body.position.z };
 
     // move camera
@@ -188,7 +188,7 @@ export class CharacterControls {
   }
 
   joyDirectionOffset(joyValues) {
-    var directionOffset = 0;
+    var directionOffset = 0; // w
 
     if (joyValues.forward > 0) {
       if (joyValues.right > 0.25) {
@@ -245,10 +245,10 @@ world.solver = new CANNON.SplitSolver(solver);
 // use this to test non-split solver
 // world.solver = solver
 world.gravity.set(0, -20, 0)
-// Create a contact material (friction coefficient = 0.0)
+// Create a contact material (friction coefficient = 0.3a)
 var physicsMaterial = new CANNON.Material('physics');
 const physics_physics = new CANNON.ContactMaterial(physicsMaterial, physicsMaterial, {
-  friction: 0.0,
+  friction: 0.3,
   restitution: 0.0,
 });
 // We must add the contact materials to the world
@@ -256,20 +256,24 @@ world.addContactMaterial(physics_physics);
 
 // CAMERA
 const camera = new THREE.PerspectiveCamera(
-  45,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  100
 );
-camera.position.y = 10;
-camera.position.z = 20;
-camera.position.x = 0;
+camera.position.set(0, 10000, 100);
 
 // RENDERER
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.enabled = true;
+
+// FOG
+const color = 0xa8def0;
+const near = 10;
+const far = 50;
+scene.fog = new THREE.Fog(color, near, far);
 
 // CONTROLS
 const orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -290,12 +294,11 @@ generateFloor();
 var gLoader = new GLTFLoader();
 
 var characterControls, guy, animationsMap = new Map(), body;
-gLoader.load("./assets/computer_guy.glb", (gltf) => {
+gLoader.load("./assets/comp_guy.glb", (gltf) => {
   gltf.scene.traverse(function (object) {
     if (object.isMesh) object.castShadow = true;
   });
   guy = gltf.scene.children[0];
-  guy.rotateY(Math.PI);
   guy.position.set(0, 1, 0);
   scene.add(guy);
   camera.position.add(guy.position);
@@ -312,6 +315,7 @@ gLoader.load("./assets/computer_guy.glb", (gltf) => {
   body.addShape(shape);
   body.position.copy(guy.position);
   body.lastPosition = { x: body.position.x, y: body.position.y, z: body.position.z };
+  body.linearDamping = 0.999;
   world.addBody(body);
 
   const mixer = new THREE.AnimationMixer(guy);
@@ -330,37 +334,127 @@ gLoader.load("./assets/computer_guy.glb", (gltf) => {
   );
 });
 
-var building, cylinder, squareBuildingBody, cylinderBuildingBody;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const textureLoader = new THREE.TextureLoader();
+const groundTexture = textureLoader.load("./assets/grass_texture_01.jpg");
+const placeholder = textureLoader.load("./assets/asphalt_road.png");
+
+var building, cylinder, sphere, pyramid, bush1, bush2, tree1, tree2;
 gLoader.load("./assets/building01.glb", (gltf) => {
   gltf.scene.traverse(function (object) {
     if (object.isMesh) object.castShadow = true;
   });
   building = gltf.scene.getObjectByName( 'square_building' );
-  building.position.set(-10,4,0);
-  scene.add(building);
-
-  cylinder = gltf.scene.getObjectByName( 'cylinder_building' )
-  cylinder.position.set(10,4,0);
-  scene.add(cylinder);
-
-  const shape1 = new CANNON.Box(new CANNON.Vec3(4, 4, 4));
-  squareBuildingBody = new CANNON.Body({
-    mass: 0,
-    type: CANNON.Body.KINEMATIC,
-    position: new CANNON.Vec3(building.position.x, building.position.y, building.position.z),
-  });
-  squareBuildingBody.addShape(shape1);
-  world.addBody(squareBuildingBody);
+  cylinder = gltf.scene.getObjectByName( 'cylinder_building' );
+  sphere = gltf.scene.getObjectByName( 'sphere_building' );
+  pyramid = gltf.scene.getObjectByName( 'pyramid_building' );
+  bush1 = gltf.scene.getObjectByName( 'bush' );
+  bush2 = gltf.scene.getObjectByName( 'bush2' );
+  tree1 = gltf.scene.getObjectByName( 'tree' );
+  tree2 = gltf.scene.getObjectByName( 'tree2' );
   
-  const shape2 = new CANNON.Box(new CANNON.Vec3(4, 4, 4));
-  cylinderBuildingBody = new CANNON.Body({
+
+  createRegion(0,0);
+  regionsList.push(current_region);
+});
+
+function createRegion(x, z) {
+  if (regionsList.filter(region => x === region.x && z === region.z).length < 1) {
+    console.log('CREATED NEW REGION', x, z);
+    createBlock(x-(REGION_SIZE/6), z-(REGION_SIZE/6));
+    createBlock(x-(REGION_SIZE/6), z+(REGION_SIZE/6));
+    createBlock(x+(REGION_SIZE/6), z+(REGION_SIZE/6));
+    createBlock(x+(REGION_SIZE/6), z-(REGION_SIZE/6));
+    createBlock(x-(REGION_SIZE/2), z-(REGION_SIZE/2));
+    createBlock(x-(REGION_SIZE/2), z+(REGION_SIZE/2));
+    createBlock(x+(REGION_SIZE/2), z+(REGION_SIZE/2));
+    createBlock(x+(REGION_SIZE/2), z-(REGION_SIZE/2));
+    createBlock(x-(REGION_SIZE/6), z-(REGION_SIZE/2));
+    createBlock(x-(REGION_SIZE/2), z+(REGION_SIZE/6));
+    createBlock(x+(REGION_SIZE/6), z+(REGION_SIZE/2));
+    createBlock(x+(REGION_SIZE/2), z-(REGION_SIZE/6));
+    createBlock(x-(REGION_SIZE/2), z-(REGION_SIZE/6));
+    createBlock(x-(REGION_SIZE/6), z+(REGION_SIZE/2));
+    createBlock(x+(REGION_SIZE/2), z+(REGION_SIZE/6));
+    createBlock(x+(REGION_SIZE/6), z-(REGION_SIZE/2));
+    addGround(x,z);
+    return true;
+  }
+  return false;
+}
+
+function createBlock(x, z) {
+  let offset = 5;
+  let buildingX = x + (getRandomInt(2) ? 1 : -1) * offset, buildingZ = z + (getRandomInt(2) ? 1 : -1) * offset;
+  createBuilding(BUILDING_TYPES[getRandomInt(4)], buildingX , 0, buildingZ);
+  for (let i = 0; i < getRandomInt(25); i++){
+    let foliageX = x + ((getRandomInt(2) ? 1 : -1) * getRandomInt(10)), foliageZ = z + ((getRandomInt(2) ? 1 : -1) * getRandomInt(10));
+    if (!((foliageX < buildingX + offset && foliageX > buildingX - offset) && (foliageZ < buildingZ + offset && foliageZ > buildingZ - offset))) {
+      createFoliage(FOLIAGE_TYPES[getRandomInt(4)], foliageX, 0, foliageZ);
+    }
+  }
+}
+
+function createBuilding(buildingType, x, y, z) {
+  let newBuilding;
+  switch (buildingType) {
+    case 'cylinder':
+      newBuilding = cylinder.clone();
+      break;
+    case 'sphere':
+      newBuilding = sphere.clone();
+      break;
+    case 'pyramid':
+      newBuilding = pyramid.clone();
+      break;
+    case 'square':
+    default:
+      newBuilding = building.clone();
+  }
+  newBuilding.position.set(x,y,z);
+  scene.add(newBuilding);
+  let buildingShape = new CANNON.Box(new CANNON.Vec3(3.25, 3.25, 3.25));
+  let buildingBody = new CANNON.Body({
     mass: 0,
     type: CANNON.Body.KINEMATIC,
-    position: new CANNON.Vec3(cylinder.position.x, cylinder.position.y, cylinder.position.z),
+    position: new CANNON.Vec3(newBuilding.position.x, newBuilding.position.y, newBuilding.position.z),
   });
-  cylinderBuildingBody.addShape(shape2);
-  world.addBody(cylinderBuildingBody);
-});
+  buildingBody.addShape(buildingShape);
+  world.addBody(buildingBody);
+}
+
+function createFoliage(foliageType, x, y, z) {
+  let newFoliage;
+  switch (foliageType) {
+    case 'tree1':
+      newFoliage = tree1.clone();
+      break;
+    case 'tree2':
+      newFoliage = tree2.clone();
+      break;
+    case 'bush1':
+      newFoliage = bush1.clone();
+      break;
+    case 'bush2':
+    default:
+      newFoliage = bush2.clone();
+  }
+  newFoliage.position.set(x,y,z);
+  scene.add(newFoliage);
+  if (foliageType === 'tree1' || foliageType === 'tree2') {
+    let foliageShape = new CANNON.Box(new CANNON.Vec3(0.25, 2, 0.25));
+    let foliageBody = new CANNON.Body({
+      mass: 0,
+      type: CANNON.Body.KINEMATIC,
+      position: new CANNON.Vec3(newFoliage.position.x, newFoliage.position.y, newFoliage.position.z),
+    });
+    foliageBody.addShape(foliageShape);
+    world.addBody(foliageBody);
+  }
+}
 
 // KEYBOARD CONTROLS
 const keysPressed = {};
@@ -450,6 +544,8 @@ function animate() {
     guy.position.copy(body.position);
     body.quaternion.copy(guy.quaternion);
   }
+  updateCurrentRegion();
+  proceduralWorldUpdate();
   orbitControls.update();
   renderer.render(scene, camera);
   world.step(timeStep, mixerUpdateDelta);
@@ -474,21 +570,6 @@ function onWindowResize() {
 window.addEventListener("resize", onWindowResize);
 
 function generateFloor() {
-  // Ground Render
-  const WIDTH = 80;
-  const LENGTH = 80;
-
-  const textureLoader = new THREE.TextureLoader();
-  const placeholder = textureLoader.load("./assets/grass_texture_01.jpg");
-  const geometry = new THREE.PlaneGeometry(WIDTH, LENGTH, 512, 512);
-  var material_floor = new THREE.MeshNormalMaterial();
-  const material = new THREE.MeshPhongMaterial({ map: placeholder });
-
-  const floor = new THREE.Mesh(geometry, material);
-  floor.receiveShadow = true;
-  floor.rotation.x = -Math.PI / 2;
-  scene.add(floor);
-
   // Ground Physics
   const groundMaterial = new CANNON.Material('ground')
   groundMaterial.friction = 0.3
@@ -498,6 +579,120 @@ function generateFloor() {
   groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
   groundBody.position.set(0, -1, 0);
   world.addBody(groundBody)
+}
+
+function addGround(x,z) {
+  var geometry = new THREE.PlaneGeometry(REGION_SIZE, REGION_SIZE, 512, 512);
+  const material = new THREE.MeshPhongMaterial({ map: groundTexture });
+
+  const ground = new THREE.Mesh(geometry, material);
+  ground.receiveShadow = true;
+  ground.position.set(x,0,z);
+  ground.rotation.x = -Math.PI / 2;
+  scene.add(ground);
+
+  const roadMaterial = new THREE.MeshPhongMaterial({ map: placeholder });
+
+  geometry = new THREE.PlaneGeometry(10, REGION_SIZE, 512, 512);
+  const floor = new THREE.Mesh(geometry, roadMaterial);
+  floor.position.set(x,0.02,z);
+  floor.receiveShadow = true;
+  floor.rotation.x = -Math.PI / 2;
+  scene.add(floor);
+
+  geometry = new THREE.PlaneGeometry(REGION_SIZE, 10, 512, 512);
+  const floor2 = new THREE.Mesh(geometry, roadMaterial);
+  floor2.position.set(x,0.01,z);
+  floor2.receiveShadow = true;
+  floor2.rotation.x = -Math.PI / 2;
+  scene.add(floor2);
+}
+
+function proceduralWorldUpdate() {
+  console.log(current_region.x, current_region.z, current_region);
+  if (body.position.z < current_region.z) {
+    if (createRegion(current_region.x, current_region.z - REGION_SIZE)) {
+      let newRegion = {
+        x: current_region.x,
+        z: current_region.z - REGION_SIZE,
+        bottom: current_region
+      }
+      current_region.top = newRegion;
+      checkForConnectedRegions(newRegion);
+      regionsList.push(newRegion);
+    }
+  }
+  if (body.position.z > current_region.z) {
+    if (createRegion(current_region.x, current_region.z + REGION_SIZE)) {
+      let newRegion = {
+        x: current_region.x,
+        z: current_region.z + REGION_SIZE,
+        top: current_region
+      }
+      current_region.bottom = newRegion;
+      checkForConnectedRegions(newRegion);
+      regionsList.push(newRegion);
+    }
+  }
+  if (body.position.x < current_region.x) {
+    if (createRegion(current_region.x - REGION_SIZE, current_region.z)){
+      let newRegion = {
+        x: current_region.x - REGION_SIZE,
+        z: current_region.z,
+        right: current_region
+      }
+      current_region.left = newRegion;
+      checkForConnectedRegions(newRegion);
+      regionsList.push(newRegion);
+    }
+  }
+  if (body.position.x > current_region.x) {
+    if (createRegion(current_region.x + REGION_SIZE, current_region.z)) {
+      let newRegion = {
+        x: current_region.x + REGION_SIZE,
+        z: current_region.z,
+        left: current_region
+      }
+      current_region.right = newRegion;
+      checkForConnectedRegions(newRegion);
+      regionsList.push(newRegion);
+    }
+  }
+}
+
+function updateCurrentRegion() {
+  if (body.position.x > current_region.x + 42) {
+    current_region = current_region.right;
+  } else if (body.position.x < current_region.x - 42) {
+    current_region = current_region.left;
+  }
+  if (body.position.z > current_region.z + 42) {
+    current_region = current_region.bottom;
+  } else if (body.position.z < current_region.z - 42) {
+    current_region = current_region.top;
+  }
+}
+
+function checkForConnectedRegions(newRegion) {
+  let regionRight = regionsList.filter(region => (region.x === newRegion.x + REGION_SIZE && region.z === newRegion.z));
+  let regionLeft = regionsList.filter(region => (region.x === newRegion.x - REGION_SIZE && region.z === newRegion.z));
+  let regionBottom = regionsList.filter(region => (region.x === newRegion.x && region.z === newRegion.z + REGION_SIZE));
+  let regionTop = regionsList.filter(region => (region.x === newRegion.x && region.z === newRegion.z - REGION_SIZE));
+
+  console.log(regionRight, regionLeft, regionTop, regionBottom);
+
+  if (regionLeft && newRegion.left === undefined) {
+    newRegion.left = regionLeft[0];
+  }
+  if (regionRight && newRegion.right === undefined) {
+    newRegion.right = regionRight[0];
+  }
+  if (regionTop && newRegion.top === undefined) {
+    newRegion.top = regionTop[0];
+  }
+  if (regionBottom && newRegion.bottom === undefined) {
+    newRegion.bottom = regionBottom[0];
+  }
 }
 
 function light() {
